e74090bd9519368c5298d19048b627c357e01f0c
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-08-05 14:13:05 +0300

    finishing cc17

--- Trees/Trees/Program.cs
+++ Trees/Trees/Program.cs
@@ -6,6 +6,19 @@
 		{
 			Console.WriteLine("Hello, World!");
 			//BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+			BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+			binarySearchTree.Add(50);
+			binarySearchTree.Add(30);
+			binarySearchTree.Add(70);
+			binarySearchTree.Add(20);
+			binarySearchTree.Add(40);
+			binarySearchTree.Add(60);
+
+			// Act
+
+			List<int> list = binarySearchTree.BreadthFirst();
+			
+			Console.WriteLine(binarySearchTree.PrintBreadthFirst(list));
 		}
 	}
 }
