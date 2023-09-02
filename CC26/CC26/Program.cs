f98b15b059e918a62f88da21fa466c9b4e523a26
alaaabusalem <alaa.abusalem94@hotmail.com>
2023-09-02 17:43:42 +0300

    cc6

---
+++ CC26/CC26/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using CC26;
+
+Console.WriteLine("Hello, World!");
+
+int[] input = { 4, 2, 7, 1, 9, 5 };
+
+Sorting sortedArray = new Sorting();
+int[] sorted = sortedArray.InsertionSort(input);
+
+Console.WriteLine("Sorted array:");
+foreach (int num in sorted)
+{
+	Console.Write(num + " ");
+}
